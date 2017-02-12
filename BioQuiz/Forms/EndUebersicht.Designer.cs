71847cdeb21043e1f7016b44f0f93dc64365396a
Steeginaldo <Steeginaldo@users.noreply.github.com>
2017-02-12 20:43:33 +0100

    DataGridView für das Erbebnis

--- BioQuiz/Forms/EndUebersicht.Designer.cs
+++ BioQuiz/Forms/EndUebersicht.Designer.cs
@@ -29,25 +29,59 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Frage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Richtig_Falsch = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(98, 9);
+            this.label1.Location = new System.Drawing.Point(171, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Dein Ergebnis";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Frage,
+            this.Richtig_Falsch});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(367, 212);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Frage
+            // 
+            this.Frage.HeaderText = "Frage";
+            this.Frage.Name = "Frage";
+            this.Frage.ReadOnly = true;
+            // 
+            // Richtig_Falsch
+            // 
+            this.Richtig_Falsch.HeaderText = "Richtig/Falsch";
+            this.Richtig_Falsch.Name = "Richtig_Falsch";
+            this.Richtig_Falsch.ReadOnly = true;
+            // 
             // EndUebersicht
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(530, 371);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "EndUebersicht";
             this.Text = "Ergebnis";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +90,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Frage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Richtig_Falsch;
     }
 }
